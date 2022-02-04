86888c96a3f999ea141fa7d19a17faedd997200c
Eva Medvedeva <94213526+elnadra@users.noreply.github.com>
2022-02-04 17:06:59 +0300

    Create water.c

---
+++ water.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int min(int a) {
+    int bottle;
+    bottle = a * 12;
+    return(bottle);
+}
+
+int main(void) {
+    int a;
+    printf("Введите число: ");
+        scanf("%i", &a);
+    
+    while(a < 0 || a == 0) {
+        printf("Введите число: ");
+        scanf("%i", &a);
+      
+    }
+   if(a > 0) {
+        printf("Вы израсходовали %i бутылок воды за %i минут!\n", min(a), a);
+   }
+   else {
+       printf("Ошибка!\n");
+       
+   }
+}
+
